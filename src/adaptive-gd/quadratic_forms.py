8741efdc8d06be1489c2f44f0c1e6e30fd8ba545
BlueSkyAndSomeCurses <65008462+BlueSkyAndSomeCurses@users.noreply.github.com>
2025-12-04 22:55:21 +0200

    feature: experiments for quadratic form adaptive g.d. with jax

--- src/adaptive-gd/quadratic_forms.py
+++ src/adaptive-gd/quadratic_forms.py
@@ -1,12 +1,15 @@
 import marimo
 
-__generated_with = "0.18.1"
+__generated_with = "0.18.2"
 app = marimo.App(width="medium")
 
 
 @app.cell
 def _():
+    import jax
+
     import numpy as np
+    import jax.numpy as jnp
 
     from numpy.typing import NDArray
 
@@ -14,45 +17,62 @@ def _():
     import plotly.express as px
 
     from scipy.stats import ortho_group
-    return NDArray, np, ortho_group
+    return NDArray, jax, jnp, np, ortho_group
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    # Random quadratic form generation
+    """)
+    return
 
 
 @app.cell
 def _(NDArray, NDarray, np, ortho_group):
     def gen_eigenvalues(
-        min_ev: float = 0.0, max_ev: float = 1e9, dim: int = 3, null_space_dim: int = 0
-    ) -> NDArray[np.float64]:
+            min_ev: float = 0.0, max_ev: float = 1e9, dim: int = 3, null_space_dim: int = 0
+        ) -> NDArray[np.float64]:
         non_zero_evs = np.random.uniform(low=min_ev, high=max_ev, size=dim - null_space_dim)
         return np.hstack((np.zeros(null_space_dim), non_zero_evs))
 
 
     def gen_eigenvectors(dim: int = 3) -> NDarray[np.float64]:
         return ortho_group.rvs(dim=dim)
+    return gen_eigenvalues, gen_eigenvectors
 
 
-    def gen_quadratic_form(
-        min_ev: float = 0.0, max_ev: float = 1e9, dim: int = 3, null_space_dim: int = 0
-    ) -> tuple[NDarray[np.float64], NDArray[np.float64]]:
-        if null_space_dim > dim:
-            raise ValueError("Null space dimension cannot be greater than dimensions of matrix")
+@app.cell
+def _(NDArray, gen_eigenvalues, gen_eigenvectors, jnp, np):
+    class QuadraticForm:
+        def __init__(self, min_ev: float = 0.0, max_ev: float = 1e9, dim: int = 3, null_space_dim: int = 0) -> None:
+            self.eigen_vals = np.sort(gen_eigenvalues(min_ev=min_ev, max_ev=max_ev, dim=dim))
+            jordan_form = np.diag(self.eigen_vals)
 
-        eigen_vals = gen_eigenvalues(min_ev=min_ev, max_ev=max_ev, dim=dim)
-        jordan_form = np.diag(eigen_vals)
+            eigen_vecs = gen_eigenvectors(dim=dim)
+            inv_eigen_vec = np.linalg.inv(eigen_vecs)
 
-        eigen_vecs = gen_eigenvectors(dim=dim)
-        inv_eigen_vec = np.linalg.inv(eigen_vecs)
+            self.b = np.random.rand(dim)
 
-        b = np.random.rand(dim)
-
-        A = eigen_vecs @ jordan_form @ inv_eigen_vec
-        return A, b
-    return (gen_quadratic_form,)
+            self.A = eigen_vecs @ jordan_form @ inv_eigen_vec
+        
+        def convert_to_jax(self) -> None:
+            self.A = jnp.array(self.A)
+            self.b = jnp.array(self.b)
+        
+        def calculate_gradient(self, x: NDArray | jnp.ndarray) -> NDArray | jnp.ndarray:
+            return self.A @ x - self.b
+        
+        def calculate_function(self, x: NDArray | jnp.ndarray) -> NDArray | jnp.ndarray:
+            return 0.5 * x.T @ self.A @ x - self.b @ x
+        
+    return (QuadraticForm,)
 
 
 @app.cell
 def _(mo):
-    min_ev = mo.ui.slider(start=0, stop=100, step=0.1)
-    max_ev = mo.ui.slider(start=0, stop=100, step=0.1)
+    min_ev = mo.ui.slider(start=0, stop=1000, step=0.1)
+    max_ev = mo.ui.slider(start=0, stop=1000, step=0.1)
     dim = mo.ui.slider(start=0, stop=100, step=1)
     null_space_dim = mo.ui.slider(start=0, stop=100, step=1)
     return dim, max_ev, min_ev, null_space_dim
@@ -72,14 +92,123 @@ def _(dim, max_ev, min_ev, mo, null_space_dim):
 
 
 @app.cell
-def _(dim, gen_quadratic_form, max_ev, min_ev, null_space_dim):
-    A1, b1 = gen_quadratic_form(min_ev.value, max_ev.value, dim.value, null_space_dim.value)
-    return A1, b1
+def _(QuadraticForm, dim, max_ev, min_ev, null_space_dim):
+    f1 = QuadraticForm(min_ev.value, max_ev.value, dim.value, null_space_dim.value)
+    return (f1,)
 
 
 @app.cell
-def _(A1, b1):
-    A1, b1
+def _(f1):
+    f1.A, f1.b, f1.eigen_vals
+    return
+
+
+@app.cell
+def _(mo):
+    mo.md(r"""
+    # Convergence test
+    For step $ \frac{\nabla f(x) ^ \top \nabla f(x)}{\nabla f(x) ^ \top A\nabla f(x)} $, experimentaly test whether it convegece to $ \frac{1}{L} $
+    """)
+    return
+
+
+@app.cell
+def _(mo):
+    lower_dim = mo.ui.slider(start=0, stop=100, step=1)
+    upper_dim = mo.ui.slider(start=0, stop=100, step=1)
+    initial_point_samples_num = mo.ui.slider(start=1, stop=100, step=1)
+    gd_num_iterations = mo.ui.slider(start = 100, stop = 1000, step = 1)
+    return gd_num_iterations, initial_point_samples_num, lower_dim, upper_dim
+
+
+@app.cell
+def _(
+    gd_num_iterations,
+    initial_point_samples_num,
+    lower_dim,
+    max_ev,
+    mo,
+    upper_dim,
+):
+    mo.vstack(
+        [
+            mo.hstack([mo.md("Maximum eigenvalue"), max_ev]),
+            mo.hstack([mo.md("Starting dimension of quadratic form"), lower_dim]),
+            mo.hstack([mo.md("Maximum dimension of quadratic form"), upper_dim]),
+            mo.hstack([mo.md("Initial point samples per quadratic form"), initial_point_samples_num]),
+            mo.hstack([mo.md("G.D. number of iterations"), gd_num_iterations]),
+        ]
+    )
+    return
+
+
+@app.cell
+def _(jnp):
+    def quadratic_gradient(A: jnp.ndarray, b: jnp.ndarray, x: jnp.ndarray) -> jnp.ndarray:
+        return A @ x - b
+    
+    def quadratic_function(A: jnp.ndarray, b: jnp.ndarray, x: jnp.ndarray) -> jnp.ndarray:
+        return 0.5 * x.T @ A @ x - b.T @ x
+    return
+
+
+@app.cell
+def _():
+    device = "cpu"
+    return (device,)
+
+
+@app.cell
+def _(
+    QuadraticForm,
+    device,
+    gd_num_iterations,
+    initial_point_samples_num,
+    jax,
+    jnp,
+    lower_dim,
+    max_ev,
+    upper_dim,
+):
+    with jax.default_device(jax.devices(device)[0]):
+        for curr_dim in range(lower_dim.value, upper_dim.value + 1):
+            min_ev_random, max_ev_random = jnp.sort(jax.random.uniform(
+                key = jax.random.PRNGKey(curr_dim),
+                shape=(2,),
+                minval=0,
+                maxval=max_ev.value,
+            ))
+            for null_space_dim_curr in range(curr_dim // 2):
+                f = QuadraticForm(min_ev_random, max_ev_random, curr_dim, null_space_dim_curr)
+                f.convert_to_jax()
+            
+                print("================================================================")
+                print(f"Generated quadratic form of dimension {curr_dim} and kernel dimension {null_space_dim_curr}")
+                print(f"Maximum eigen value {f.eigen_vals[-1]}")
+                print(f"1/λ {1 / f.eigen_vals[-1]}")
+                print("================================================================")
+            
+                for _ in range(initial_point_samples_num.value):
+                    print("---")
+                    print("Generated new starting point")
+                    print("---")
+                    x = jax.random.uniform(
+                        key = jax.random.PRNGKey(curr_dim),
+                        shape=(curr_dim,),
+                        minval=-100,
+                        maxval=100,
+                    )
+                
+                    for i in range(gd_num_iterations.value):
+                        grad_i = f.calculate_gradient(x)
+                        lambda_i = (grad_i.T @ grad_i) / (grad_i.T @ f.A @grad_i)
+                    
+                        print(f"Iteration {i}. Learning rate: {lambda_i} ")
+                    
+                        x -= lambda_i * grad_i
+                    
+
+                    
     return
 
 
