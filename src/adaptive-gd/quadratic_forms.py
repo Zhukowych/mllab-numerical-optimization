1960a1739a4995ce2dbd5831864217daaf707467
BlueSkyAndSomeCurses <65008462+BlueSkyAndSomeCurses@users.noreply.github.com>
2025-12-09 20:50:55 +0200

    experiments for specific step choice

--- src/adaptive-gd/quadratic_forms.py
+++ src/adaptive-gd/quadratic_forms.py
@@ -1,6 +1,6 @@
 import marimo
 
-__generated_with = "0.18.2"
+__generated_with = "0.18.1"
 app = marimo.App(width="medium")
 
 
@@ -18,23 +18,8 @@ def _():
 
     from scipy.stats import ortho_group
 
-    import wandb
-    from decouple import config
-
     import polars as pl
-    return NDArray, config, jax, jnp, np, ortho_group, pl, wandb
-
-
-@app.cell
-def _(config):
-    wadnb_api_key = config("WANDB_API_KEY")
-    return (wadnb_api_key,)
-
-
-@app.cell
-def _(wadnb_api_key, wandb):
-    wandb.login(key=wadnb_api_key)
-    return
+    return NDArray, jax, jnp, np, ortho_group, pl, px
 
 
 @app.cell
@@ -123,7 +108,7 @@ def _(f1):
 def _(mo):
     mo.md(r"""
     # Convergence test
-    For step $ \frac{\nabla f(x) ^ \top \nabla f(x)}{\nabla f(x) ^ \top A\nabla f(x)} $, experimentaly test whether it convegece to $ \frac{1}{L} $
+    For step $\frac{\nabla f(x) ^ \top \nabla f(x)}{\nabla f(x) ^ \top A\nabla f(x)}$, experimentaly test whether it convegece to $\frac{1}{L}$
     """)
     return
 
@@ -133,7 +118,7 @@ def _(mo):
     lower_dim = mo.ui.slider(start=0, stop=100, step=1)
     upper_dim = mo.ui.slider(start=0, stop=100, step=1)
     initial_point_samples_num = mo.ui.slider(start=1, stop=100, step=1)
-    gd_num_iterations = mo.ui.slider(start = 100, stop = 1000, step = 1)
+    gd_num_iterations = mo.ui.slider(start = 100, stop = 3000, step = 1)
     return gd_num_iterations, initial_point_samples_num, lower_dim, upper_dim
 
 
@@ -160,7 +145,7 @@ def _(
 
 @app.cell
 def _():
-    device = "cpu"
+    device = "cuda"
     return (device,)
 
 
@@ -191,24 +176,25 @@ def _(
     jnp,
     lower_dim,
     max_ev,
+    mo,
     results,
     upper_dim,
 ):
     with jax.default_device(jax.devices(device)[0]):
-        for curr_dim in range(lower_dim.value, upper_dim.value + 1):
+        for curr_dim in mo.status.progress_bar(range(lower_dim.value, upper_dim.value + 1), title="Dimension of function", show_eta=True, show_rate=True):
             min_ev_random, max_ev_random = jnp.sort(jax.random.uniform(
                 key = jax.random.PRNGKey(curr_dim),
                 shape=(2,),
                 minval=0,
                 maxval=max_ev.value,
             ))
-            for null_space_dim_curr in range(curr_dim // 2):
+            for null_space_dim_curr in (0, 5):
                 f = QuadraticForm(min_ev_random, max_ev_random, curr_dim, null_space_dim_curr)
                 f.convert_to_jax()
-            
-            
 
-                for init_point_idx in range(initial_point_samples_num.value):
+
+
+                for init_point_idx in mo.status.progress_bar(range(initial_point_samples_num.value), title="Initial point", show_eta=True, show_rate=True):
                     x = jax.random.uniform(
                         key = jax.random.PRNGKey(curr_dim),
                         shape=(curr_dim,),
@@ -216,25 +202,27 @@ def _(
                         maxval=100,
                     )
 
-                    for i in range(gd_num_iterations.value):
-                        grad_i = f.calculate_gradient(x)
-                        lambda_i = (grad_i.T @ grad_i) / (grad_i.T @ f.A @grad_i)
+                    with mo.status.spinner(subtitle="Descending...") as _spinner:
+                        for i in range(gd_num_iterations.value):
+                            grad_i = f.calculate_gradient(x)
+                            lambda_i = (grad_i.T @ grad_i) / (grad_i.T @ f.A @grad_i)
 
-                        if (i % 100 == 0):
-                        
-                            results["dimension"].append(curr_dim)
-                            results["kernel_size"].append(null_space_dim_curr)
-                            results["max_eigenvalue"].append(f.eigen_vals[-1])
-                            results["min_eigenvalue"].append(f.eigen_vals[null_space_dim_curr])
-                            results["max_eigenvalue_inverse"].append(1 / f.eigen_vals[-1])
-                            results["initial_point_index"].append(init_point_idx)
-                            results["iteration"].append(i)
-                            results["function_value"].append(f.calculate_function(x))
-                            results["gradient_norm"].append(jnp.linalg.norm(grad_i))
-                            results["learning_rate"].append(lambda_i)
+                            if (i % 100 == 0):
 
-                        x -= lambda_i * grad_i
-            
+                                results["dimension"].append(curr_dim)
+                                results["kernel_size"].append(null_space_dim_curr)
+                                results["max_eigenvalue"].append(f.eigen_vals[-1])
+                                results["min_eigenvalue"].append(f.eigen_vals[null_space_dim_curr])
+                                results["max_eigenvalue_inverse"].append(1 / f.eigen_vals[-1])
+                                results["initial_point_index"].append(init_point_idx)
+                                results["iteration"].append(i)
+                                results["function_value"].append(f.calculate_function(x))
+                                results["gradient_norm"].append(jnp.linalg.norm(grad_i))
+                                results["learning_rate"].append(lambda_i)
+
+                                _spinner.update(f"max_ev: {f.eigen_vals[-1]}, min_ev: {f.eigen_vals[null_space_dim_curr]}, max_ev_inv: {1 / f.eigen_vals[-1]}, lr: {lambda_i}") 
+
+                            x -= lambda_i * grad_i
     return
 
 
@@ -246,7 +234,89 @@ def _(pl, results):
 
 @app.cell
 def _(results_df):
-    results_df.write("quadratic_forms_gd_results.parquet")
+    results_df.write_parquet("quadratic_forms_gd_results_3000_iters.parquet")
+    return
+
+
+@app.cell
+def _(pl):
+    results_df_loaded = pl.read_parquet("quadratic_forms_gd_results_3000_iters.parquet")
+    return (results_df_loaded,)
+
+
+@app.cell
+def _(mo, results_df_loaded):
+    mo.ui.dataframe(results_df_loaded)
+    return
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    Hypothesis whether learning rate converges to $\frac{2}{\lambda_{\min} + \lambda_{\max}}$
+    """)
+    return
+
+
+@app.cell
+def _(pl, results_df_loaded):
+    results_lr_hyp = results_df_loaded.with_columns(
+    pl.lit(2).truediv(pl.col("max_eigenvalue").add(pl.col("min_eigenvalue"))).alias("best_lr")
+    ).with_columns(
+        pl.col("learning_rate").sub(pl.col("best_lr")).abs().alias("lr_error")
+    ).group_by("dimension", "kernel_size", "initial_point_index").agg("iteration", "gradient_norm", "learning_rate", "best_lr", "lr_error").explode("iteration", "gradient_norm", "learning_rate", "best_lr", "lr_error")
+    return (results_lr_hyp,)
+
+
+@app.cell
+def _(mo, results_lr_hyp):
+    mo.ui.dataframe(results_lr_hyp)
+    return
+
+
+@app.cell
+def _(mo, results_lr_hyp):
+    dim_to_plot = mo.ui.dropdown(options=results_lr_hyp["dimension"].unique())
+    ker_to_plot = mo.ui.dropdown(options=results_lr_hyp["kernel_size"].unique())
+    init_point_to_plot = mo.ui.dropdown(options=results_lr_hyp["initial_point_index"].unique())
+    what_to_plot = mo.ui.dropdown(options=["gradient_norm", "learning_rate", "best_lr", "lr_error"])
+    return dim_to_plot, init_point_to_plot, ker_to_plot, what_to_plot
+
+
+@app.cell
+def _(dim_to_plot, init_point_to_plot, ker_to_plot, mo, what_to_plot):
+    mo.vstack(
+        [
+            mo.hstack([mo.md("Dim of function"), dim_to_plot]),
+            mo.hstack([mo.md("Kernel size of function"), ker_to_plot]),
+            mo.hstack([mo.md("Initial point index"), init_point_to_plot]),
+            mo.hstack([mo.md("What to plot on y-axis?"), what_to_plot]),
+        ]
+    )
+    return
+
+
+@app.cell
+def _(
+    dim_to_plot,
+    init_point_to_plot,
+    ker_to_plot,
+    pl,
+    px,
+    results_lr_hyp,
+    what_to_plot,
+):
+    chosen_data = results_lr_hyp.filter(pl.col("dimension").eq(dim_to_plot.value) 
+                                        & pl.col("kernel_size").eq(ker_to_plot.value)
+                                        & pl.col("initial_point_index").eq(init_point_to_plot.value) 
+                                        & pl.col("iteration").ge(500)
+                                       )
+    y_axis = what_to_plot.value
+    y_min, y_max = chosen_data[y_axis].min(), chosen_data[y_axis].max()
+
+    px.scatter(
+        chosen_data, x="iteration", y=y_axis, range_y=[y_min, y_max]
+    )
     return
 
 
@@ -254,6 +324,11 @@ def _(results_df):
 def _():
     import marimo as mo
     return (mo,)
+
+
+@app.cell
+def _():
+    return
 
 
 if __name__ == "__main__":
