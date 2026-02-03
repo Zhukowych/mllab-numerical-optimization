2ee7113fc61c03f30e7cb86caac7a01750a6da0c
MaVasTarn <vasyl.tarnavskyi@botscrew.com>
2026-02-03 20:20:10 +0200

    fix

--- src/adaptive-gd/adg_vs_optimal.py
+++ src/adaptive-gd/adg_vs_optimal.py
@@ -249,8 +249,8 @@ def _(Path, QuadraticForm, jax, jnp, mo, np, pl):
 
                             x_prev = x
                             grad_prev = grad_i
-                            lambda_prev = lambda_i
                             theta_i = lambda_i / lambda_prev
+                            lambda_prev = lambda_i
 
                             x -= lambda_i * grad_i
 
@@ -695,6 +695,32 @@ def _(distribution_experiment, pl, preprocess_data):
 
 
 @app.cell
+def _(adg_data, optimal_step_data, pl):
+    adg_with_optimal = (
+        adg_data.join(
+            optimal_step_data.select(
+                "dimension",
+                "kernel_size",
+                "initial_point_index",
+                "iteration",
+                pl.col("loss").name.suffix("_optimal"),
+                pl.col("learning_rate").name.suffix("_optimal"),
+                pl.col("loss_rate_of_change").name.suffix("_optimal"),
+            ),
+            on=("dimension", "kernel_size", "initial_point_index", "iteration"),
+            how="left",
+        )
+        .with_columns(
+            pl.when(pl.col("learning_rate_optimal").is_not_null())
+            .then(pl.col("learning_rate") / pl.col("learning_rate_optimal"))
+            .otherwise(None)
+            .alias("step_ratio")
+        )
+    )
+    return (adg_with_optimal,)
+
+
+@app.cell
 def _(adg_data, convergence_threshold, mo, optimal_step_data, pl):
     adg_data_convergence = adg_data.group_by("dimension", "kernel_size", "initial_point_index").agg(pl.col("loss").min())
     optimal_step_data_convergence = optimal_step_data.group_by("dimension", "kernel_size", "initial_point_index").agg(
@@ -731,24 +757,9 @@ def _(mo):
 
 
 @app.cell
-def _(adg_data, convergence_threshold, optimal_step_data, pl, wilcoxon):
+def _(adg_with_optimal, convergence_threshold, pl, wilcoxon):
     hypotheses_test: pl.DataFrame = (
-        adg_data.join(
-            optimal_step_data.select(
-                "dimension",
-                "kernel_size",
-                "initial_point_index",
-                "iteration",
-                pl.col("loss").name.suffix("_optimal"),
-                pl.col("learning_rate").name.suffix("_optimal"),
-                pl.col("loss_rate_of_change").name.suffix("_optimal")
-            ),
-            on=("dimension", "kernel_size", "initial_point_index", "iteration"),
-            how="left",
-        )
-        .with_columns(
-            (pl.col("learning_rate") / pl.col("learning_rate_optimal")).alias("step_ratio")
-        )
+        adg_with_optimal
         .filter(
             (pl.col("loss").ge(convergence_threshold.value) | pl.col("loss_optimal").ge(convergence_threshold.value))
             & (
@@ -834,6 +845,7 @@ def _(dim_to_plot, init_point_to_plot, ker_to_plot, mo, what_to_plot):
 @app.cell
 def _(
     adg_data,
+    adg_with_optimal,
     dim_to_plot,
     distribution_experiment,
     go,
@@ -853,6 +865,12 @@ def _(
         & pl.col("initial_point_index").eq(init_point_to_plot.value)
     )
     right_chosen_data = optimal_step_data.filter(
+        pl.col("dimension").eq(dim_to_plot.value)
+        & pl.col("kernel_size").eq(ker_to_plot.value)
+        & pl.col("initial_point_index").eq(init_point_to_plot.value)
+    )
+
+    chosen_joined_data = adg_with_optimal.filter(
         pl.col("dimension").eq(dim_to_plot.value)
         & pl.col("kernel_size").eq(ker_to_plot.value)
         & pl.col("initial_point_index").eq(init_point_to_plot.value)
@@ -903,10 +921,18 @@ def _(
         go.Histogram(x=right_chosen_data["learning_rate"], nbinsx=50, name="optimal step distribution"), row=1, col=2
     )
 
+    step_ratio_figure = make_subplots(rows=1, cols=1)
+    step_ratio_figure.add_trace(
+        go.Histogram(x=chosen_joined_data["step_ratio"], nbinsx=50, name="step ratio (ADG/optimal)"),
+        1,
+        1,
+    )
+
 
     gd_results_plot = mo.ui.plotly(fig)
     lr_distribution_plot = mo.ui.plotly(lr_distribution_figure)
-    return eigen_vals_plot, gd_results_plot, lr_distribution_plot
+    step_ratio_plot = mo.ui.plotly(step_ratio_figure)
+    return eigen_vals_plot, gd_results_plot, lr_distribution_plot, step_ratio_plot
 
 
 @app.cell
@@ -914,10 +940,11 @@ def _(
     eigen_vals_plot,
     gd_results_plot,
     lr_distribution_plot,
+    step_ratio_plot,
     mo,
     plot_constructor,
 ):
-    mo.vstack([plot_constructor, gd_results_plot, eigen_vals_plot, lr_distribution_plot])
+    mo.vstack([plot_constructor, gd_results_plot, eigen_vals_plot, lr_distribution_plot, step_ratio_plot])
     return
 
 
